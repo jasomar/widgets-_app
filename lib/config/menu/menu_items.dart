c8dfccf9b236c1ac1d7d93dbaddd7c6151eba041
Jafet Solano <jasomar1998@gmail.com>
2026-04-25 21:04:00 -0600

    home screen and menu items added

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: "botones",
+    subTitle: "subTitle",
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: "Cards",
+    subTitle: "subTitle",
+    link: '/card',
+    icon: Icons.credit_card,
+  ),
+];
