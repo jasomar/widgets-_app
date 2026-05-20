e8b6bdd0e0f9e9a104183cb16bd3bcb53ca01d08
Jafet Solano <jasomar1998@gmail.com>
2026-05-19 22:58:30 -0600

    pre-config buttons added

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const name = "buttons_screen";
@@ -9,7 +10,50 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("buttons screen")),
-      body: const Placeholder(),
+      body: const _buttonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _buttonsView extends StatelessWidget {
+  const _buttonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 10,
+        ),
+        child: Wrap(
+          spacing: 10,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Elevate"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+
+              child: const Text("Elevate Disabled"),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.access_alarms_rounded),
+              label: Text("Elevate icon test"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
