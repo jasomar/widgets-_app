1eb3f4adaa4ba766f22d34ad34a9fd866c7178be
Jafet Solano <jasomar1998@gmail.com>
2026-08-02 21:13:58 -0600

    cards added

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'elevation 0'},
+  {'elevation': 1.0, 'label': 'elevation 1'},
+  {'elevation': 2.0, 'label': 'elevation 2'},
+  {'elevation': 3.0, 'label': 'elevation 3'},
+  {'elevation': 4.0, 'label': 'elevation 4'},
+  {'elevation': 5.0, 'label': 'elevation 5'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const name = "cards_screen";
   const CardsScreen({super.key});
@@ -8,7 +17,46 @@ class CardsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Cards screen")),
-      body: const Placeholder(),
+      body: _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ...cards.map(
+          (card) => _CardType1(
+            elevation: card['elevation'],
+            label: card['label'],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType1({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [Icon(Icons.more_vert)]),
+      ),
     );
   }
 }
