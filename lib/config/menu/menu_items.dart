55ebaf1aa50b00d4426e82525abe1a825f5db3a4
Jafet Solano <jasomar1998@gmail.com>
2026-08-31 20:07:38 -0600

    animated container

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -39,4 +39,10 @@ const appMenuItems = <MenuItem>[
     link: '/snackbars',
     icon: Icons.info_outline,
   ),
+  MenuItem(
+    title: "Animated Container",
+    subTitle: "Animated",
+    link: '/animated',
+    icon: Icons.check_box_outlined,
+  ),
 ];
