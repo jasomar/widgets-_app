16cb916d98bcee77dfb7d2618fd9003def64cc98
Jafet Solano <jasomar1998@gmail.com>
2026-09-16 21:21:35 -0600

    tutorial completed

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -27,10 +28,43 @@ class SlideInfo {
   SlideInfo(this.title, this.caption, this.imageUrl);
 }
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const name = 'AppTutorialScreen';
 
   const AppTutorialScreen({super.key});
+
+  @override
+  State<AppTutorialScreen> createState() =>
+      _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState
+    extends State<AppTutorialScreen> {
+  final PageController pageviewController =
+      PageController();
+
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageviewController.addListener(() {
+      final page = pageviewController.page ?? 0;
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +73,7 @@ class AppTutorialScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
+            controller: pageviewController,
             physics: const BouncingScrollPhysics(),
             children: slides
                 .map(
@@ -58,6 +93,21 @@ class AppTutorialScreen extends StatelessWidget {
               child: Text("Skip "),
             ),
           ),
+
+          endReached
+              ? Positioned(
+                  right: 30,
+                  bottom: 30,
+                  child: FadeInRight(
+                    from: 15,
+                    delay: const Duration(seconds: 1),
+                    child: FilledButton(
+                      onPressed: () => context.pop(),
+                      child: Text("Start"),
+                    ),
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
     );
